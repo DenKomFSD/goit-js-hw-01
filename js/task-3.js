7b71121894c0bece6b988274ca7573b17e381ee0
iziKomSD <122015463+iziKomSD@users.noreply.github.com>
2023-12-08 18:42:10 +0200

    1

--- js/task-3.js
+++ js/task-3.js
@@ -1,17 +1,5 @@
-function getElementWidth(content, padding, border) {
-    let contentWidth = parseFloat(content);
-    let paddingWidth = parseFloat(padding);
-    let borderWidth = parseFloat(border);
-    let totalWidth = contentWidth + paddingWidth * 2 + borderWidth * 2;
-    return totalWidth;
-    
-}
+'use strict';
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
-
-// Variant 2
 function getElementWidth(content, padding, border) {
     let contentWidth = parseFloat(content);
     let paddingWidth = parseFloat(padding) * 2;
